8f8a58986bdd80a81a25e25cda9dcbc03af178a3
timothyleslie <836241487@qq.com>
2020-11-11 17:42:18 +0800

    完成框架

--- Lab1/lexer.py
+++ Lab1/lexer.py
@@ -1,9 +1,19 @@
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 alphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-          'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+          'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+keywords = ['auto', 'double', 'int', 'struct', 'break', 'else', 'long', 'switch', 'case', 'enum',
+            'register', 'typedef', 'char', 'extern', 'return', 'union', 'const', 'float', 'short', 'unsigned',
+            'continue', 'for', 'signed', 'void', 'default', 'goto', 'sizeof', 'volatile', 'do', 'if',
+            'while', 'static']
 
-global current_state
-global States
+boadwords = [';', ',', '(', ')', '.', '{', '}', '[', ']']
+
+current_state = 10
+msg = ""
+buf = ""
+character_table = []
 
 
 def generate_states():
@@ -28,20 +38,158 @@ def is_alpha(ch):
 
 
 def analysis(ch):
-    global current_state
     global States
-    if current_state == States[0]:
-        if is_alpha(ch):
-            current_state = States[14]
-        elif is_digit(ch):
-            current_state = States[16]
+    global current_state
+    global msg
+    global buf
+    global character_table
+    print(ch)
+    while True:
+        # print(ch)
+        # print(current_state)
+        if current_state == States[0]:
+            if is_alpha(ch):
+                buf = buf + ch
+                current_state = States[1]
+                return
+            elif is_digit(ch):
+                buf = buf + ch
+                current_state = States[3]
+                return
+            elif ch == '\'':
+                current_state = States[7]
+                return
+            elif ch == '\"':
+                current_state = States[9]
+                return
+            elif ch == ',':
+                current_state = States[11]
+            elif ch == ';':
+                current_state = States[12]
+            elif ch == '(':
+                current_state = States[13]
+            elif ch == ')':
+                current_state = States[14]
+            elif ch == '{':
+                current_state = States[15]
+            elif ch == '}':
+                current_state = States[16]
+            elif ch == '[':
+                current_state = States[17]
+            elif ch == ']':
+                current_state = States[18]
+            else:
+                current_state = States[0]
+                return
 
+        elif current_state == States[1]:
+            if is_alpha(ch) or is_digit(ch):
+                buf = buf + ch
+                return
+            else:
+                current_state = States[2]
+
+        elif current_state == States[2]:
+            if buf in keywords:
+                msg = msg + '(' + buf.upper() + ', ' + buf + ')\n'
+                # print(msg)
+
+            else:
+                msg = msg + '(ID, ' + buf + ')\n'
+                if buf not in character_table:
+                    character_table.append(buf)
+                # print(msg)
+
+            buf = ""
+            current_state = States[0]
+
+        elif current_state == States[3]:
+            if is_digit(ch):
+                buf = buf + ch
+                current_state = States[3]
+                return
+            elif ch == '.':
+                buf = buf + ch
+                current_state = States[5]
+                return
+            else:
+                current_state = States[4]
+
+        elif current_state == States[4]:
+            msg = msg + '(Int, ' + buf + ')\n'
+            buf = ""
+            current_state = States[0]
+
+        elif current_state == States[5]:
+            if is_digit(ch):
+                buf = buf + ch
+                current_state = States[5]
+                return
+            else:
+                current_state = States[6]
+
+        elif current_state == States[6]:
+            msg = msg + '(Real, ' + buf + ')\n'
+            buf = ""
+            current_state = States[0]
+
+        elif current_state == States[7]:
+            if ch == '\'':
+                current_state = States[8]
+            else:
+                buf = buf + ch
+                current_state = States[7]
+                return
+
+        elif current_state == States[8]:
+            msg = msg + '(Char ' + buf + ')\n'
+            buf = ""
+            current_state = States[0]
+            return
+
+        elif current_state == States[9]:
+            if ch == '\"':
+                current_state = States[10]
+            else:
+                buf = buf + ch
+                current_state = States[9]
+                return
+
+        elif current_state == States[10]:
+            msg = msg + '(String ' + buf + ')\n'
+            buf = ""
+            current_state = States[0]
+            return
+
+        # ","
+        elif current_state == States[11]:
+            msg = msg + '(DOT, ,)\n'
+            current_state = States[0]
+            return
+
+        # ";"
+        elif current_state == States[12]:
+            msg = msg + '(SEMIC, ;)\n'
+            current_state = States[0]
+            return
+
+        # "("
+        elif current_state == States[13]:
+            msg = msg + ''
 
 
 def scanner(text):
+    global msg
+    global character_table
     text_length = len(text)
     for i in range(0, text_length):
         analysis(text[i])
+    with open('token.txt', 'w') as token_file:
+        token_file.write(msg)
+    with open('charactertable.txt', 'w') as character_file:
+        for character in character_table:
+            character_file.write(character)
+            character_file.write('\n')
 
 
 def main():
@@ -50,7 +198,8 @@ def main():
     States = generate_states()
     current_state = States[0]
     fp = open('test.c', 'r')
-    scanner(fp.open)
+    fp = fp.read()
+    scanner(fp)
 
 
 if __name__ == '__main__':
---
+++ Lab1/test.c
@@ -0,0 +1,6 @@
+int s;
+int result;
+s = 10.00;
+result = s + 2*s+(s-4)
+char ch = 'abc'
+String str = "hello world"
