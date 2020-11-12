e5b735372f93947f221993000b5608e69a1a9327
timothyleslie <836241487@qq.com>
2020-11-12 18:49:01 +0800

    final vrsion

--- Lab1/lexer.py
+++ Lab1/lexer.py
@@ -1,6 +1,6 @@
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 alphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-          'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_',
+          'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
           'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 EncodeTable = {'auto': (1, "AUTO"), 'double': (2, "DOUBLE"), 'int': (3, "INT"), 'struct': (4, "STRUCT"),
@@ -12,12 +12,13 @@ EncodeTable = {'auto': (1, "AUTO"), 'double': (2, "DOUBLE"), 'int': (3, "INT"), 
                'continue': (21, "CONTINUE"), 'for': (22, "FOR"), 'signed': (23, "SIGNED"), 'void': (24, "VOID"),
                'default': (25, "DEFAULT"), 'goto': (26, "GOTO"), 'sizeof': (27, "SIZEOF"),
                'volatile': (28, "VOLATILE"), 'do': (29, "DO"), 'if': (30, "IF"),
-               'while': (31, "WHILE"), 'static': (32, "STATIC"), ';': (33, "SEMIC"), ',': (34, "DOT"),
-               '(': (35, "OPEN_PAREN"), ')': (36, "CLOSE_PAREN"), '{': (38, "OPEN_CURLY"), '}': (39, "CLOSE_CURLY"),
+               'while': (31, "WHILE"), 'static': (32, "STATIC"), ';': (33, "SEMIC"), ',': (34, "COMMA"),
+               '(': (35, "OPEN_PAREN"), ')': (36, "CLOSE_PAREN"), '.': (37, "DOT"), '{': (38, "OPEN_CURLY"),
+               '}': (39, "CLOSE_CURLY"),
                '[': (40, "OPEN_BRACKET"), ']': (41, "CLOSE_BRACKET"),
                '+': (42, "PLUS"), '-': (43, "MINUS"), '*': (44, "MULTI"), '/': (45, "DIVIDE"), '!': (46, "NOT"),
-               '|': (47, "OR"), '&': (48, "AND"), '=': (49, "ASSIGN"), '==': (50, "EQUAL"), '<': (51, "LE"),
-               '>': (52, "GE"), '<=': (53, "LEQ"), '>=': (54, "GEQ")}
+               '|': (47, "BIT_OR"), '&': (48, "BIT_AND"), '=': (49, "ASSIGN"), '==': (50, "EQUAL"), '<': (51, "LE"),
+               '>': (52, "GE"), '<=': (53, "LEQ"), '>=': (54, "GEQ"), '&&': (59, "AND"), '||': (60, "OR")}
 ID = (0, "ID")
 INT = (55, "INT_NUM")
 REAL = (56, "REAL_NUM")
@@ -26,7 +27,7 @@ STRING = (58, "STRING_CONST")
 current_state = 10
 msg = ""
 buf = ""
-character_table = {}
+symbol_table = {}
 
 
 def generate_states():
@@ -55,7 +56,7 @@ def analysis(ch):
     global current_state
     global msg
     global buf
-    global character_table
+    global symbol_table
     # print(ch)
     while True:
         # print(ch)
@@ -103,8 +104,10 @@ def analysis(ch):
                 current_state = States[23]
             elif ch == '|':
                 current_state = States[24]
+                return
             elif ch == '&':
                 current_state = States[25]
+                return
             elif ch == '=':
                 current_state = States[26]
                 return
@@ -114,26 +117,27 @@ def analysis(ch):
             elif ch == '>':
                 current_state = States[28]
                 return
+            elif ch == '.':
+                current_state = States[39]
             else:
                 current_state = States[0]
                 return
 
         elif current_state == States[1]:
-            if is_alpha(ch) or is_digit(ch):
+            if is_alpha(ch) or is_digit(ch) or ch == '_':
                 buf = buf + ch
                 return
             else:
                 current_state = States[2]
 
         elif current_state == States[2]:
-            if buf in EncodeTable:
+            if buf in EncodeTable.keys():
                 msg = msg + '(' + str(EncodeTable[buf][1]) + ', ' + buf + ')\n'
-                # print(msg)
 
             else:
-                if buf not in character_table.keys():
-                    character_table[buf] = len(character_table)
-                msg = msg + '(' + ID[1] + ', ' + str(character_table[buf]) + ')\n'
+                if buf not in symbol_table.keys():
+                    symbol_table[buf] = len(symbol_table)
+                msg = msg + '(' + ID[1] + ', ' + str(symbol_table[buf]) + ')\n'
 
             buf = ""
             current_state = States[0]
@@ -276,15 +280,17 @@ def analysis(ch):
 
         # "|"
         elif current_state == States[24]:
-            msg = msg + '(' + str(EncodeTable['|'][1]) + ', ' + '|' + ')\n'
-            current_state = States[0]
-            return
+            if ch == '|':
+                current_state = States[35]
+            else:
+                current_state = States[36]
 
         # "&"
         elif current_state == States[25]:
-            msg = msg + '(' + str(EncodeTable['&'][1]) + ', ' + '&' + ')\n'
-            current_state = States[0]
-            return
+            if ch == '&':
+                current_state = States[37]
+            else:
+                current_state = States[38]
 
         # "="
         elif current_state == States[26]:
@@ -337,20 +343,46 @@ def analysis(ch):
             msg = msg + '(' + str(EncodeTable['>'][1]) + ', ' + '>' + ')\n'
             current_state = States[0]
 
+        # ||
+        elif current_state == States[35]:
+            msg = msg + '(' + str(EncodeTable['||'][1]) + ', ' + '||' + ')\n'
+            current_state = States[0]
+            return
+
+        elif current_state == States[36]:
+            msg = msg + '(' + str(EncodeTable['|'][1]) + ', ' + '|' + ')\n'
+            current_state = States[0]
+
+        # &&
+        elif current_state == States[37]:
+            msg = msg + '(' + str(EncodeTable['&&'][1]) + ', ' + '&&' + ')\n'
+            current_state = States[0]
+            return
+
+        elif current_state == States[38]:
+            msg = msg + '(' + str(EncodeTable['&'][1]) + ', ' + '&' + ')\n'
+            current_state = States[0]
+
+        # .
+        elif current_state == States[39]:
+            msg = msg + '(' + str(EncodeTable['.'][1]) + ', ' + '.' + ')\n'
+            current_state = States[0]
+            return
+
 
 def scanner(text):
     global msg
-    global character_table
+    global symbol_table
     text_length = len(text)
     for i in range(0, text_length):
         analysis(text[i])
 
     with open('token.txt', 'w') as token_file:
         token_file.write(msg)
-    with open('charactertable.txt', 'w') as character_file:
-        for character in character_table.items():
+    with open('symbol_table.txt', 'w') as character_file:
+        for character in symbol_table.items():
             character_file.write(str(character[1]))
-            character_file.write(',')
+            character_file.write(':\t')
             character_file.write(character[0])
             character_file.write('\n')
 
--- Lab1/test.c
+++ Lab1/test.c
@@ -1,6 +1,10 @@
-int s;
+float s;
 int result;
 s = 10.00;
 result = s + 2*s+(s-4)
 char ch = 'abc'
-String str = "hello world"
+char str[20] = "hello world"
+
+,;(){}[]+-*/!|&=<>
+||&&==>=<=
+.
